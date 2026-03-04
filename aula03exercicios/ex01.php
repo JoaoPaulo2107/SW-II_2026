c52babe55b8a829e9ed115df6ad7a600dcb16f7f
JoaoPaulo2107 <161509047+JoaoPaulo2107@users.noreply.github.com>
2026-03-03 23:36:56 -0300

    Realização de exercícios

---
+++ aula03exercicios/ex01.php
@@ -0,0 +1,13 @@
+<?php
+    $nome = "joão";
+    $idade = 17;
+    $cidade = "Rio Grande da Serra";
+
+
+    echo "Nome: $nome <br>";
+    echo "Idade: $idade <br>";
+    echo "Cidade: $cidade <br>";
+
+
+
+?>
---
+++ aula03exercicios/ex07.php
@@ -0,0 +1,8 @@
+<?php
+    $nomes = ["John Wick","Arthur Morgan","Jon Snow","Deacon ST. John", "Kratos", "Tyrion Lannister"];
+
+    foreach ($nomes as $nome) {
+        echo "$nome<br>";
+    }
+
+?>
